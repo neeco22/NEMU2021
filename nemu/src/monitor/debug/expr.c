0bbccd01a4c0d36f5a20ab3e86c85411de6ca9e6
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-03 11:53:46 +0800

    >  compile NEMU 2020244008 neeco Linux DESKTOP-QK64DUL 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  11:53:46 up  1:47,  1 user,  load average: 0.00, 0.01, 0.00 74f1660eddefaad45b5e6ac4cfdc95af52ab16

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -212,9 +212,9 @@ uint32_t expr(char *e, bool *success) {
 		*success = false;
 		return 0;
 	}
+	*success=true;
+	return (uint32_t)eval(0,nr_token-1);
 	
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
-	return 0;
 }
 
