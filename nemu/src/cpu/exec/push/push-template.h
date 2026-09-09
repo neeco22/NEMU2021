20b631d705e1a80e26d0efb1f787aa18c01b1996
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-09 15:05:19 +0800

    >  compile NEMU 2020244008 neeco Linux DESKTOP-QK64DUL 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  15:05:19 up  4:28,  1 user,  load average: 0.06, 0.07, 0.01 df14042c566a5e7c1bad79242460e8d08c431fce

--- nemu/src/cpu/exec/push/push-template.h
+++ nemu/src/cpu/exec/push/push-template.h
@@ -4,8 +4,8 @@
 
 static void do_execute(){
     cpu.esp-=DATA_BYTE;
-    swaddr_write(cpu.esp,DATA_BYTE,op_src->val);
-    print_asm(str(instr) str(SUFFIX) " %s",op_src->str);
+    MEM_W(cpu.esp,op_src->val);
+    print_asm_template1();
 }
 
 make_instr_helper(r);
