5bdd85f02d11bce6a33e69b7d6ec91e9e222c3e6
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-10 11:52:53 +0800

    >  compile NEMU 2020244008 neeco Linux DESKTOP-QK64DUL 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  11:52:53 up  1:29,  1 user,  load average: 0.37, 0.20, 0.20 93e1f4074dad420ec5373705ad156189816b8a4e

---
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -0,0 +1,14 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute(){
+    DATA_TYPE val=MEM_R(cpu.esp);
+    cpu.esp+=DATA_BYTE;
+    OPERAND_W(op_src,val);
+    print_asm_template1();
+}
+
+make_instr_helper(r);
+
+#include "cpu/exec/template-end.h"
