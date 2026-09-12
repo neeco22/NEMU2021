d244117efa590411814e232c0eed084f3afef0e0
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-12 10:10:24 +0800

    >  compile NEMU 2020244008 neeco Linux DESKTOP-QK64DUL 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  10:10:24 up 12 min,  1 user,  load average: 0.52, 0.24, 0.10 f18da72b6fd4828a295cc7d0278cb45be258588b

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -10,4 +10,19 @@
         return 2;\
     }
 
-make_jcc(je, cpu.eflags.ZF == 1)
+make_jcc(jo,   cpu.eflags.OF)
+make_jcc(jno,  !cpu.eflags.OF)
+make_jcc(jb,   cpu.eflags.CF)
+make_jcc(jae,  !cpu.eflags.CF)
+make_jcc(je,   cpu.eflags.ZF)                          
+make_jcc(jne,  !cpu.eflags.ZF)
+make_jcc(jbe,  cpu.eflags.CF || cpu.eflags.ZF)
+make_jcc(ja,   !cpu.eflags.CF && !cpu.eflags.ZF)
+make_jcc(js,   cpu.eflags.SF)
+make_jcc(jns,  !cpu.eflags.SF)
+make_jcc(jp,   cpu.eflags.PF)
+make_jcc(jnp,  !cpu.eflags.PF)
+make_jcc(jl,   cpu.eflags.SF != cpu.eflags.OF)
+make_jcc(jge,  cpu.eflags.SF == cpu.eflags.OF)
+make_jcc(jle,  cpu.eflags.ZF || (cpu.eflags.SF != cpu.eflags.OF))
+make_jcc(jg,   !cpu.eflags.ZF && (cpu.eflags.SF == cpu.eflags.OF))
--- nemu/src/cpu/exec/control/jcc.h
+++ nemu/src/cpu/exec/control/jcc.h
@@ -1,6 +1,21 @@
 #ifndef __JCC_H__
 #define __JCC_H__
 
+make_helper(jo);
+make_helper(jno);
+make_helper(jb);
+make_helper(jae);
 make_helper(je);
+make_helper(jne);
+make_helper(jbe);
+make_helper(ja);
+make_helper(js);
+make_helper(jns);
+make_helper(jp);
+make_helper(jnp);
+make_helper(jl);
+make_helper(jge);
+make_helper(jle);
+make_helper(jg);
 
 #endif
