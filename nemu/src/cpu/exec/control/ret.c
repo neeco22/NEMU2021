3ec77744587e301593cc7d1aee3161e6fdf9f25d
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-13 14:39:47 +0800

    >  compile NEMU 2020244008 neeco Linux DESKTOP-QK64DUL 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:39:47 up 18 min,  1 user,  load average: 0.02, 0.11, 0.08 b47505efab68f5dbbb1d35757e8746e3b6e4be9

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -6,3 +6,11 @@ make_helper(ret) {
     print_asm("ret");
     return 0;
 }
+
+make_helper(ret_iw){
+    uint16_t imm=swaddr_read(cpu.esp+1,2);
+    cpu.eip=swaddr_read(cpu.esp,4);
+    cpu.esp+=4+imm;
+    print_asm("ret %x",imm);
+    return 0;
+}
