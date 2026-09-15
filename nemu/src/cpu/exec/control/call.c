e98c1d707187b3fac4a963e09705c80a81cafbc6
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-15 11:53:52 +0800

    >  compile NEMU 2020244008 neeco Linux DESKTOP-QK64DUL 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  11:53:52 up  1:57,  1 user,  load average: 0.44, 0.15, 0.07 b5f534f749efc4f5a4c6680937d0bdf56e2146

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -3,3 +3,12 @@
 #define DATA_BYTE 4
 #include "call-template.h"
 #undef DATA_BYTE
+
+make_helper(call_rm_l){
+    int len=decode_rm_l(eip + 1);
+    cpu.esp -= 4;
+    swaddr_write(cpu.esp,4,eip + 1 + len);
+    cpu.eip = op_src->val;
+    print_asm("call *%s",op_src->str);
+    return 0;
+}
--- nemu/src/cpu/exec/control/call.h
+++ nemu/src/cpu/exec/control/call.h
@@ -2,5 +2,6 @@
 #define __CALL_H__
 
 make_helper(call_si_l);
+make_helper(call_rm_l);
 
 #endif
